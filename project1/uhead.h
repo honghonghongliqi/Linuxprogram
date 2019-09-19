1b99bf6928c328e9f2057dd63bf7544223eddf5c
honghonghongliqi <34413246+honghonghongliqi@users.noreply.github.com>
2019-09-19 14:41:42 +0800

    Update uhead.h

--- project1/uhead.h
+++ project1/uhead.h
@@ -13,6 +13,7 @@ struct result{
 void show(int *,int);//打印结果
 void uround(int *,int);//产生随机数,size为数量
 stuct result uoprater(int *,int);//计算总数和平均数
+int comp(const void *a,const void *b);//qsort()用函数
 void syssort(int *,int);//系统的排序
 void usort(int *,int);//自己的排序
 double calculate_time();//计算时间差的函数
