6c6f56b2efdcc9ae349053349f2e615f88f7a1f8
honghonghongliqi <34413246+honghonghongliqi@users.noreply.github.com>
2019-09-26 15:16:20 +0800

    Update change_main.c

--- project2/change_main.c
+++ project2/change_main.c
@@ -10,17 +10,17 @@ int main(){
     if((fp_old=fopen("/etc/passwd","r"))==NULL){//利用fopen以只读形式打开文件
         exit(1);
     }
-    if((fp_new=open(des,"a+"))==NULL){//以可读可写追加方式打开一个文本文件若文件不存在，则会建立该文件。
+    if((fp_new=fopen(des,"a+"))==NULL){//以可读可写追加方式打开一个文本文件若文件不存在，则会建立该文件。
         exit(1);
     }
     while((nbytes=fread(buf,sizeof(*buf),20,fp_old))>0){//fread(保存数据的指针,每个数据类型的大小,数据的数量,文件指针).
-        z=write(buf,sizeof(*buf),nbytes,fp_new);//fwrite(保存数据的指针,每个数据类型的大小,数据的数量,文件指针).
+        z=fwrite(buf,sizeof(*buf),nbytes,fp_new);//fwrite(保存数据的指针,每个数据类型的大小,数据的数量,文件指针).
         if(z<0){
             perror("写目标文件出错");/*此函数可以用来输出“错误原因信息”字符串*/
         }
     }
     fclose(fp_old);
-    close(fp_new);
+    fclose(fp_new);
     printf("复制“/etc/passwd”文件为“%s”文件成功!\n",des);
     exit(0);
 }
