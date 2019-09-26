772f554c014e84a3d4aae375210be8ce163c6b2c
honghonghongliqi <34413246+honghonghongliqi@users.noreply.github.com>
2019-09-26 16:33:55 +0800

    Update fun3.c

--- project1/fun3.c
+++ project1/fun3.c
@@ -1,8 +1,8 @@
 #include"uhead.h"
 
 void uwrite(const char *file_name,int *array,int size){
-    int *handle;
-    if((handle=open(file_name,O_WRONLY | O_CREATE | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO))==-1){
+    int handle;
+    if((handle=open(file_name,O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO))==-1){
         printf("Error!\n");
         exit(1);
     }
@@ -13,7 +13,7 @@ void uwrite(const char *file_name,int *array,int size){
 }
 
 void uread(const char *file_name,int *array,int size){
-    int *handle;
+    int handle;
     if((handle=open(file_name,O_RDONLY,S_IWRITE|S_IREAD))==-1){
         printf("Error!\n");
         exit(1);
