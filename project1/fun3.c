e972c5fc0eaf1daa44304b21d336e2cf6c2766c9
honghonghongliqi <289656921@qq.com>
2019-09-29 22:31:40 +0800

    update

--- project1/fun3.c
+++ project1/fun3.c
@@ -2,7 +2,7 @@
 
 void uwrite(const char *file_name,int *array,int size){
     int handle;
-    if((handle=open(file_name,O_WRONLY | O_CREAT | O_TRUNC , S_IRWXU | S_IRWXG | S_IRWXO))==-1){
+    if((handle=open(file_name,O_WRONLY | O_CREAT | O_TRUNC ,0664))==-1){
         printf("Error!\n");
         exit(1);
     }
