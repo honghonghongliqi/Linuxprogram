254287e1833c1ed1ea336a08b9e592641e399bd1
honghonghongliqi <289656921@qq.com>
2019-10-15 11:10:12 +0800

    finish

---
+++ code/exam/remotetestmodi.c
@@ -0,0 +1,10 @@
+#include"../staticlib/staticlib.h"
+int main()
+{
+   int a[5];
+   initarray(a,5);
+   showarray(a,5);
+   printf("max= %4d\n",max(a,5));
+   printf("sum= %4d\n",sum(a,5));
+   return 0;
+}
