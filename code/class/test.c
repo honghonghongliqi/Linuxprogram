f329d08caf908769726c2f006572c4199d473eeb
honghonghongliqi <289656921@qq.com>
2019-10-29 09:06:33 +0800

    finsh

---
+++ code/class/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+   printf("ppid= %d,pid= %d\n",getppid(),getpid());
+   while(1);
+   return 0;
+}
--- code/dynamiclib/dylib.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int max(int *,int);
-int sum(int *,int);
-void init(int *,int);
-void show(int *,int);
-
