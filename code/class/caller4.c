a942816e5d58a2796d2fd05772f2a44f62541b4f
honghonghongliqi <289656921@qq.com>
2019-11-06 16:02:57 +0800

    test

--- code/class/caller4.c
+++
@@ -1,9 +0,0 @@
-#include "my.h"
-int main()
-{
-    char *arg[]={"./test","123","hello","World",NULL};
-    printf("caller2:pid = %d,ppid = %d\n",getpid(),getppid());
-    execv("/home/wyz/linuxprogram/code/test",arg);
-    printf("excel after calling!\n");
-    return 0;
-}
