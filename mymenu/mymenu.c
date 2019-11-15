4ffd58152f412c0ba0b811664c65e18c528711f4
honghonghongliqi <289656921@qq.com>
2019-11-15 17:01:04 +0800

    静态链接库测试

---
+++ mymenu/mymenu.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void print_menu()
+{
+   printf("****************************\n");
+   printf("0.退出\n");
+   printf("1.创建新文件\n");
+   printf("2.写文件\n");
+   printf("3.读文件\n");
+   printf("4.修改文件权限\n");
+   printf("5.查看当前文件的权限修改文件权限\n");
+   printf("****************************\n");
+   printf("Please input your choice(0-6):");
+}
